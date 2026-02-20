97d03fbb5e16f6ffaa133e8c53e0222bceab816d
HiQaody <hiqaody@gmail.com>
2026-02-20 23:28:27 +0300

    refactor(controller): simplify product controller by removing unnecessary decorators and adjusting the controller path

--- src/v1/products/products.controller.ts
+++ src/v1/products/products.controller.ts
@@ -21,19 +21,15 @@ import {
   ApiResponse,
   ApiBody,
   ApiBearerAuth,
-  getSchemaPath,
-  ApiExtraModels,
 } from '@nestjs/swagger';
 import { ProductService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Auth, AuthRole } from '../auth';
 import { UserAccess } from '../users/users.schema';
-import { Product } from './products.schema';
 
 @ApiTags('Produits')
-@ApiBearerAuth()
-@Controller('products')
+@Controller()
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
