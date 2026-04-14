dcbdf15c729e0f6eac19145c27de7d9074a7d631
HiQaody <hiqaody@gmail.com>
2026-04-14 21:37:04 +0300

    feat(transactions): Integrate UsersService for recipient email retrieval in transaction notifications

--- src/v1/transactions/transactions.module.ts
+++ src/v1/transactions/transactions.module.ts
@@ -8,6 +8,7 @@ import { PassifsModule } from '../passifs/passifs.module';
 import { SharedModule } from '../../shared/shared.module';
 import { ProductsModule } from '../products/products.module';
 import { StockModule } from '../stock/stock.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { StockModule } from '../stock/stock.module';
     SharedModule,
     ProductsModule,
     StockModule,
+    UsersModule,
   ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
